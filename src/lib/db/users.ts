d491403f7efba5eb6db52d9e23fba46f643515d2
skyfoxx <skyfoxx@sacred.finance>
2025-06-03 19:41:06 +1000

    feat(users): create

--- src/lib/db/users.ts
+++ src/lib/db/users.ts
@@ -2,4 +2,13 @@ import { prisma } from '../prisma';
 import type { User, Prisma } from '../../generated/prisma';
 
 export const userService = {
+    // Create a new user
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        return prisma.user.create({
+            data,
+            include: {
+                profiles: true,
+            },
+        });
+    },
 };
