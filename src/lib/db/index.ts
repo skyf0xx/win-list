d04ac81fd14e6fa129b2406e457ff2cd03dd1fe7
skyfoxx <skyfoxx@sacred.finance>
2025-06-03 19:42:25 +1000

    feat(db): export services

---
+++ src/lib/db/index.ts
@@ -0,0 +1,5 @@
+export { userService } from './users';
+export { profileService } from './profiles';
+export { categoryService } from './categories';
+export { taskService } from './tasks';
+export { prisma } from '../prisma';
