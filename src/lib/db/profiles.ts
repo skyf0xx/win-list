cfdaa2e848f431522a90adaa27b906da31409aa3
skyfoxx <skyfoxx@sacred.finance>
2025-06-03 19:38:15 +1000

    feat(profiles): create

--- src/lib/db/profiles.ts
+++ src/lib/db/profiles.ts
@@ -2,4 +2,15 @@ import { prisma } from '../prisma';
 import type { Profile, Prisma } from '../../generated/prisma';
 
 export const profileService = {
+    // Create a new profile
+    async create(data: Prisma.ProfileCreateInput): Promise<Profile> {
+        return prisma.profile.create({
+            data,
+            include: {
+                user: true,
+                tasks: true,
+                categories: true,
+            },
+        });
+    },
 };
