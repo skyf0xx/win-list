ff705aeedcce208204043d1fd5b7ac1b2925cc01
skyfoxx <skyfoxx@sacred.finance>
2025-06-03 19:31:46 +1000

    feat(categories):create

--- src/lib/db/categories.ts
+++ src/lib/db/categories.ts
@@ -2,4 +2,14 @@ import { prisma } from '../prisma';
 import type { Category, Prisma } from '../../generated/prisma';
 
 export const categoryService = {
+    // Create a new category
+    async create(data: Prisma.CategoryCreateInput): Promise<Category> {
+        return prisma.category.create({
+            data,
+            include: {
+                profile: true,
+                tasks: true,
+            },
+        });
+    },
 };
